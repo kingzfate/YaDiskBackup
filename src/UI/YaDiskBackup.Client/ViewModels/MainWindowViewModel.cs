e1a544a45381dc9b502b4a95802c5650ddfabeb3
Авраменко Никита Александрович <NikAvrggl@gmail.com>
2024-01-24 01:22:38 +0500

    Program is ready, need to refactor and fix some bugs

--- src/UI/YaDiskBackup.Client/ViewModels/MainWindowViewModel.cs
+++ src/UI/YaDiskBackup.Client/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,8 @@ using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Windows.Input;
 using YaDiskBackup.Domain.Abstractions;
@@ -15,21 +17,38 @@ namespace YaDiskBackup.Client.ViewModels;
 /// <summary>
 /// View model for main view
 /// </summary>
-public class MainWindowViewModel : NavigationViewModelBase
+public class MainWindowViewModel : ReactiveObject
 {
     private readonly ReadOnlyObservableCollection<CopiedFile> list;
     public ReadOnlyObservableCollection<CopiedFile> List => list;
 
+    //private ObservableAsPropertyHelper<bool> greeting;
+    //public bool Greeting => greeting.Value;
+
+    //public bool _stopState { get; set; }
+    //public bool _startState { get; set; }
+
+    //public bool StopState { get => _stopState; set => this.RaiseAndSetIfChanged(ref _stopState, value); }
+    //public bool StartState { get => _startState; set => this.RaiseAndSetIfChanged(ref _startState, value); }
+
+    //private ObservableAsPropertyHelper<bool> _isPaused;
+    //public bool IsPaused => _isPaused.Value;
+
+    
+
     [Reactive]
-    public bool IsPaused { get; set; } = true;
+    public string Test { get; set; }
     //private bool _paused;
     //public bool IsPaused
     //{
     //    get => _paused;
     //    set => SetAndRaise(ref _paused, value);
     //}
+
     [Reactive]
-    public bool IsRunning { get; set; } = false;
+    public bool IsPaused { get; set; } = true;
+    [Reactive]
+    public bool IsRunning { get; private set; } = false;
 
     public ICommand Browse { get; }
     public ICommand EnableBackup { get; }
@@ -50,7 +69,15 @@ public class MainWindowViewModel : NavigationViewModelBase
 
             IsPaused = false;
             IsRunning = true;
+
+            //Test = "PREDEV";
+
+            //this.WhenAnyValue(x => x.IsRunning)                
+            //    .ToPropertyEx(this, x => x.);
         });
+
+        //this.WhenAnyObservable(x => x.EnableBackup).Subscribe(x => x.);
+
 
         DisableBackup = ReactiveCommand.Create(() =>
         {
@@ -59,6 +86,10 @@ public class MainWindowViewModel : NavigationViewModelBase
             IsPaused = true;
             IsRunning = false;
         });
+
+        //this.WhenAnyValue(x => x.IsPaused, x => x.IsRunning);
+
+        //this.OneWayBind(ViewModel, vm => vm.BlServers, v => v.cmbServers.Visibility).DisposeWith(disposables);
 
         backup.Live.Connect()
             .ObserveOnDispatcher()
